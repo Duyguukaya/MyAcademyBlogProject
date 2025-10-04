656e28830569051c8e7fd870650d7712c2d4ce32
duygukayaa <dygkaya56@gmail.com>
2025-10-04 03:14:45 +0300

    .

---
+++ Blogy.DataAccess/Repositories/CategoryRepositories/CategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using Blogy.DataAccess.Context;
+using Blogy.Entity.Entities;
+using Blogy.DataAccess.Repositories.GenericRepositories;
+
+
+namespace Blogy.DataAccess.Repositories.CategoryRepositories
+{
+    public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+
+}
