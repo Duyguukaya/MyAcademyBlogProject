36f2d7403397df51e56ee00eb64a3bf191e983a4
duygukayaa <dygkaya56@gmail.com>
2025-10-10 02:07:09 +0300

    blog işlemleri yapıldı.

---
+++ Blogy.Business/Validators/BlogValidators/CreateBlogValidator.cs
@@ -0,0 +1,18 @@
+﻿using Blogy.Business.DTOs.BlogDtos;
+using FluentValidation;
+
+namespace Blogy.Business.Validators.BlogValidators
+{
+    public class CreateBlogValidator:AbstractValidator<CreateBlogDto>
+    {
+        public CreateBlogValidator()
+        {
+            RuleFor(x => x.Tittle).NotEmpty().WithMessage("Başlık Boş Bırakılmaz");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Açıklama Boş Bırakılmaz");
+            RuleFor(x => x.CoverImage).NotEmpty().WithMessage("Kapak Resmi Boş Bırakılmaz");
+            RuleFor(x => x.BlogImage1).NotEmpty().WithMessage("Blog Resmi 1 Boş Bırakılmaz");
+            RuleFor(x => x.BlogImage2).NotEmpty().WithMessage("Blog Resmi 2 Boş Bırakılmaz");
+            RuleFor(x => x.CategoryId).NotEmpty().WithMessage("Kategori Boş Bırakılmaz");
+        }
+    }
+}
--- Blogy.WebUI/Program.cs
+++ Blogy.WebUI/Program.cs
@@ -1,4 +1,5 @@
 using Blogy.Business.Mappings;
+using Blogy.Business.Services.BlogServices;
 using Blogy.Business.Services.CategoryServices;
 using Blogy.Business.Validators.CategoryValidators;
 using Blogy.DataAccess.Context;
@@ -21,6 +22,8 @@ builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 
 builder.Services.AddScoped<IBlogRepository, BlogRepository>();
+builder.Services.AddScoped<IBlogService, BlogService>();
+
 builder.Services.AddScoped<IBlogTagRepository, BlogTagRepository>();
 builder.Services.AddScoped<ISocialRepository, SocialRepository>();
 builder.Services.AddScoped<ITagRepository, TagRepository>();
