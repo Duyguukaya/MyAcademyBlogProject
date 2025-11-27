f4772ba326dbfefa07c9a1e4f95d9fd8897491ad
Duyguukaya <Duyguukaya@users.noreply.github.com>
2025-11-27 19:43:25 +0300

    about sayfası tamamlandı.

---
+++ Blogy.WebUI/Controllers/AboutController.cs
@@ -0,0 +1,16 @@
+﻿using Blogy.Entity.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Blogy.WebUI.Controllers
+{
+    public class AboutController(UserManager<AppUser> _userManager) : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var writers = await _userManager.GetUsersInRoleAsync("Writer");
+            return View(writers);
+        }
+    }
+}
